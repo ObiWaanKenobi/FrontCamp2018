e3af1128575a488e328f8c0104ef5d688861619c
Evgenii Gordienko <Evgenii_Gordienko@epam.com>
2018-11-23 14:58:52 +0300

    refactor(task2): moved babel config into a separate .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
+                use: ['babel-loader']
             },
             {
                 test: /\.(sa|sc|c)ss$/,
